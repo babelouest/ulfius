0b11f8d233dd206a2766e4cd33da00fa133d211a
Nicolas Mora <mail@babelouest.org>
2016-01-11 01:01:42 -0500

    fix indent

--- examples/simple_example/simple_example.c
+++ examples/simple_example/simple_example.c
@@ -41,38 +41,36 @@ int callback_get_cookietest (const struct _u_request * request, struct _u_respon
  * decode a u_map into a string
  */
 char * print_map(const struct _u_map * map) {
-	char * line, * to_return = NULL;
-    const char **keys, * value;
-	int len, i;
-	if (map != NULL) {
-		keys = u_map_enum_keys(map);
-		for (i=0; keys[i] != NULL; i++) {
-			value = u_map_get(map, keys[i]);
-			len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
-			line = malloc((len+1)*sizeof(char));
-			snprintf(line, (len+1), "key is %s, value is %s", keys[i], value);
-			if (to_return != NULL) {
-				len = strlen(to_return) + strlen(line) + 1;
-				to_return = realloc(to_return, (len+1)*sizeof(char));
-				if (strlen(to_return) > 0) {
-					strcat(to_return, "\n");
-				}
-			} else {
+  char * line, * to_return = NULL;
+  const char **keys, * value;
+  int len, i;
+  if (map != NULL) {
+    keys = u_map_enum_keys(map);
+    for (i=0; keys[i] != NULL; i++) {
+      value = u_map_get(map, keys[i]);
+      len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
+      line = malloc((len+1)*sizeof(char));
+      snprintf(line, (len+1), "key is %s, value is %s", keys[i], value);
+      if (to_return != NULL) {
+        len = strlen(to_return) + strlen(line) + 1;
+        to_return = realloc(to_return, (len+1)*sizeof(char));
+        if (strlen(to_return) > 0) {
+          strcat(to_return, "\n");
+        }
+      } else {
                 to_return = malloc((strlen(line) + 1)*sizeof(char));
                 to_return[0] = 0;
-			}
-			strcat(to_return, line);
-			free(line);
-		}
-		return to_return;
-	} else {
-		return NULL;
-	}
+      }
+      strcat(to_return, line);
+      free(line);
+    }
+    return to_return;
+  } else {
+    return NULL;
+  }
 }
 
 int main (int argc, char **argv) {
-  
-  y_init_logs("simple_example", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting simple_example");
   
   // Endpoint list declaration
   // The last line is mandatory to mark the end of the array
@@ -92,6 +90,8 @@ int main (int argc, char **argv) {
   struct _u_instance instance;
   instance.port = PORT;
   instance.bind_address = NULL;
+  
+  y_init_logs("simple_example", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting simple_example");
   
   // Start the framework
   if (ulfius_init_framework(&instance, endpoint_list) == U_OK) {
@@ -137,13 +137,13 @@ int callback_post_test (const struct _u_request * request, struct _u_response * 
  */
 int callback_all_test_foo (const struct _u_request * request, struct _u_response * response, void * user_data) {
   char * url_params = print_map(request->map_url), * headers = print_map(request->map_header), * cookies = print_map(request->map_cookie), 
-				* post_params = print_map(request->map_post_body), * json_params = json_dumps(request->json_body, JSON_INDENT(2));
+        * post_params = print_map(request->map_post_body), * json_params = json_dumps(request->json_body, JSON_INDENT(2));
   int len;
   len = snprintf(NULL, 0, "Hello World!\n\n  method is %s\n  url is %s\n\n  parameters from the url are \n%s\n\n  cookies are \n%s\n\n  headers are \n%s\n\n  post parameters are \n%s\n\n  json body parameters are \n%s\n\n  user data is %s\n\nclient address is %s\n\n",
-																	request->http_verb, request->http_url, url_params, cookies, headers, post_params, json_params, (char *)user_data, inet_ntoa(((struct sockaddr_in *)request->client_address)->sin_addr));
+                                  request->http_verb, request->http_url, url_params, cookies, headers, post_params, json_params, (char *)user_data, inet_ntoa(((struct sockaddr_in *)request->client_address)->sin_addr));
   response->string_body = malloc((len+1)*sizeof(char));
   snprintf(response->string_body, (len+1), "Hello World!\n\n  method is %s\n  url is %s\n\n  parameters from the url are \n%s\n\n  cookies are \n%s\n\n  headers are \n%s\n\n  post parameters are \n%s\n\n  json body parameters are \n%s\n\n  user data is %s\n\nclient address is %s\n\n",
-																	request->http_verb, request->http_url, url_params, cookies, headers, post_params, json_params, (char *)user_data, inet_ntoa(((struct sockaddr_in *)request->client_address)->sin_addr));
+                                  request->http_verb, request->http_url, url_params, cookies, headers, post_params, json_params, (char *)user_data, inet_ntoa(((struct sockaddr_in *)request->client_address)->sin_addr));
   response->status = 200;
   
   free(url_params);
@@ -176,22 +176,22 @@ int callback_get_jsontest (const struct _u_request * request, struct _u_response
  * The counter cookie is incremented every time the client reloads this url
  */
 int callback_get_cookietest (const struct _u_request * request, struct _u_response * response, void * user_data) {
-	const char * lang = u_map_get(request->map_url, "lang"), * extra = u_map_get(request->map_url, "extra"), 
-				* counter = u_map_get(request->map_cookie, "counter");
-	char new_counter[8];
+  const char * lang = u_map_get(request->map_url, "lang"), * extra = u_map_get(request->map_url, "extra"), 
+             * counter = u_map_get(request->map_cookie, "counter");
+  char new_counter[8];
   int i_counter;
-	
-	if (counter == NULL) {
-		i_counter = 0;
-	} else {
-		i_counter = strtol(counter, NULL, 10);
-		i_counter++;
-	}
-	snprintf(new_counter, 7, "%d", i_counter);
-	ulfius_add_cookie_to_response(response, "lang", lang, NULL, 0, NULL, NULL, 0, 0);
-	ulfius_add_cookie_to_response(response, "extra", extra, NULL, 0, NULL, NULL, 0, 0);
-	ulfius_add_cookie_to_response(response, "counter", new_counter, NULL, 0, NULL, NULL, 0, 0);
-	response->string_body = strdup("Cookies set");
-	
-	return U_OK;
+  
+  if (counter == NULL) {
+    i_counter = 0;
+  } else {
+    i_counter = strtol(counter, NULL, 10);
+    i_counter++;
+  }
+  snprintf(new_counter, 7, "%d", i_counter);
+  ulfius_add_cookie_to_response(response, "lang", lang, NULL, 0, NULL, NULL, 0, 0);
+  ulfius_add_cookie_to_response(response, "extra", extra, NULL, 0, NULL, NULL, 0, 0);
+  ulfius_add_cookie_to_response(response, "counter", new_counter, NULL, 0, NULL, NULL, 0, 0);
+  response->string_body = strdup("Cookies set");
+  
+  return U_OK;
 }
