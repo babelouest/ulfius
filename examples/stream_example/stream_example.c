4b1028446eba2260dbeec00f69ca696825c34724
babelouest <github@babelouest.org>
2016-04-18 13:12:19 -0400

    Stream audio file example

--- examples/stream_example/stream_example.c
+++ examples/stream_example/stream_example.c
@@ -13,6 +13,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "../../src/ulfius.h"
 
@@ -24,9 +26,13 @@
  */
 int callback_get_stream (const struct _u_request * request, struct _u_response * response, void * user_data);
 
+int callback_get_audio_stream (const struct _u_request * request, struct _u_response * response, void * user_data);
+
 ssize_t stream_data (void * cls, uint64_t pos, char *buf, size_t max);
+ssize_t stream_audio_file (void * cls, uint64_t pos, char *buf, size_t max);
 
 void free_stream_data(void * cls);
+void free_stream_audio_file(void * cls);
 
 int main (int argc, char **argv) {
   int ret;
@@ -42,7 +48,10 @@ int main (int argc, char **argv) {
   u_map_put(instance.default_headers, "Access-Control-Allow-Origin", "*");
   
   // Endpoint list declaration
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, NULL, NULL, NULL, NULL, &callback_get_stream, NULL);
+  if (argc > 1) {
+    ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, NULL, NULL, NULL, NULL, &callback_get_stream, NULL);
+  }
+  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/audio", NULL, NULL, NULL, &callback_get_audio_stream, argv[1]);
   
   // Start the framework
   ret = ulfius_start_framework(&instance);
@@ -64,11 +73,38 @@ int main (int argc, char **argv) {
 }
 
 /**
- * Callback function that put a "Hello World!" string in the response
+ * Callback function
  */
 int callback_get_stream (const struct _u_request * request, struct _u_response * response, void * user_data) {
   ulfius_set_stream_response(response, 200, stream_data, free_stream_data, -1, 32 * 1024, strdup("stream test"));
   return U_OK;
+}
+
+int callback_get_audio_stream (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  FILE * file = fopen((char *)user_data, "rb");
+  int fd;
+  struct stat buf;
+  printf("stream audio file\n");
+  
+  if (file != NULL) {
+
+    fd = fileno (file);
+    if (-1 == fd) {
+      fclose (file);
+      return U_ERROR; /* internal error */
+    }
+    if ( (0 != fstat (fd, &buf)) || (! S_ISREG (buf.st_mode)) ) {
+      /* not a regular file, refuse to serve */
+      fclose (file);
+      file = NULL;
+      return U_ERROR;
+    }
+    u_map_put(response->map_header, "Content-Type", "audio/mpeg");
+    ulfius_set_stream_response(response, 200, stream_audio_file, free_stream_audio_file, buf.st_size, 32 * 1024, file);
+    return U_OK;
+  } else {
+    return U_ERROR;
+  }
 }
 
 ssize_t stream_data (void * cls, uint64_t pos, char * buf, size_t max) {
@@ -82,7 +118,21 @@ ssize_t stream_data (void * cls, uint64_t pos, char * buf, size_t max) {
   }
 }
 
+ssize_t stream_audio_file (void * cls, uint64_t pos, char * buf, size_t max) {
+  FILE *file = cls;
+
+  (void) fseek (file, pos, SEEK_SET);
+  return fread (buf, 1, max, file);
+}
+
 void free_stream_data(void * cls) {
   printf("clean data\n");
   free(cls);
+}
+
+void free_stream_audio_file(void * cls) {
+  printf("clean file\n");
+
+  FILE *file = cls;
+  fclose (file);
 }
