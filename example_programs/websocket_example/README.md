a02177a82a69ae06d94bd70601913f64f0b6a0ed
babelouest <github@babelouest.org>
2017-05-16 15:38:11 -0400

    Improve websocket_example

---
+++ example_programs/websocket_example/README.md
@@ -0,0 +1,43 @@
+# websocket_example
+
+Run a webservice with a websocket endpoint that will print the client messages, and send 10 messages at the same time, one per seconds, then close the connection.
+
+## Compile and run
+
+### HTTP mode
+
+Run a HTTP (non secure) webservice
+
+```bash
+$ make debug
+$ LD_LIBRARY_PATH=../../src: ./websocket_example
+```
+
+### HTTPS mode
+
+You must have a certificate key and pem files available. To generate one, you can for example use the following command:
+
+```bash
+$ openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout cert.key -sha256
+```
+
+Run a HTTPS (secure) webservice
+
+```bash
+$ make debug
+$ LD_LIBRARY_PATH=../../src: ./websocket_example -https cert.key cert.pem
+```
+
+## Endpoints available:
+
+### Websocket
+
+- `GET http://localhost:9275/websocket`: Websocket endpoint
+
+### Static file
+
+- `GET http://localhost:9275/static`: will serve the files located in the `static` folder
+
+## Test the websocket
+
+Open in your browser the url [http[s]://localhost:9275/static](http://localhost:9275/static), this will open an HTML page where you can test the websockets behaviour.
--- example_programs/websocket_example/websocket_example.c
+++ example_programs/websocket_example/websocket_example.c
@@ -59,10 +59,34 @@ char * print_map(const struct _u_map * map) {
   }
 }
 
+char * read_file(const char * filename) {
+  char * buffer = NULL;
+  long length;
+  FILE * f = fopen (filename, "rb");
+  if (filename != NULL) {
+
+    if (f) {
+      fseek (f, 0, SEEK_END);
+      length = ftell (f);
+      fseek (f, 0, SEEK_SET);
+      buffer = malloc (length + 1);
+      if (buffer) {
+        fread (buffer, 1, length, f);
+      }
+      buffer[length] = '\0';
+      fclose (f);
+    }
+    return buffer;
+  } else {
+    return NULL;
+  }
+}
+
 int main(int argc, char ** argv) {
   int ret;
   struct _u_instance instance;
   struct static_file_config file_config;
+  char * cert_file = NULL, * key_file = NULL;
   
   y_init_logs("websocket_example", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting websocket_example");
   
@@ -93,55 +117,17 @@ int main(int argc, char ** argv) {
   ulfius_add_endpoint_by_val(&instance, "GET", PREFIX_STATIC, "*", 0, &callback_static_file, &file_config);
   
   // Start the framework
-  if (argc > 1 && 0 == o_strcmp(argv[1], "-https")) {
-    ret = ulfius_start_secure_framework(&instance, "-----BEGIN PRIVATE KEY-----\
-MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDr90HrswgEmln/\
-rXeNqYq0boIvas5wu27hmeHDdGGKtkCWIWGAo9GUy45xqsI4mDl3bOWS+pmb/3yi\
-+nhe+BmYHvEqUFo1JfUcVMxaNEbdd9REytMjKdOS+kkLf++BBRoZI/g8DggIu+Ri\
-dOSypk+pUECyQxROsyCrB/FgXuKbyC4QNl7fqZxMSpzw7jsWCZiwFv4pu8kMqzDG\
-2wTl/r/4STyK4Pj2TVa/JVzbZbH7VfcjT8MdMsXvKhlmPywjbqo70Hnmt3cnakYF\
-X+07ncx/5mjYYd3eSFgiNXr7WNw2rhFKtfTUcjrqSw9FDxmHFWUU76mwJyUo02N9\
-ViakSoQpAgMBAAECggEBAJp/VBwdJpzM6yxqyaJpZbXpvTeKuQw6zMjN1nIBG3SV\
-DAjAZnSxziGcffGSmoQvt0CoflAT4MuxJkwXrwSPcUKWz9Sis82kwq4AH6TYIaYU\
-NVmtazzUwAC1+2maJJjXXFUlpfy8Oypsy4ZjfvIxzmrPbuzI2t0Ej9kr5DDzL3BL\
-CWQ/U7w7y4KC0Pnq1ueIzM+UJIfvI0ldUcXHWsAnjyQzwgFBC35qDOfDTw0YUJv+\
-ElfFFcGYCA+9wlQyhM/zhAWqKgZ2mwAS6WykgbSc7j4NDjlmZwf4ZuTxbDUV1kBX\
-pPH21snqO42CFpw9hRUAA0W0XydCIfUhH8/6tH9enQECgYEA+rM9f6cUk3c7aLWs\
-hnauVqJuyGhgCkMyF9sSxgfcs87OVLNuGgaTIfwcT/7oxAY8G7sY44cbk1ZRhh7y\
-6kf01xqiJeXxBQei1qiJxMb2gukvpeY81s2Mg9og5d9qbEhLzp8TdiRJHxLIiGwF\
-xOM69CpugKN4T0Zum7EBGeSvmBECgYEA8PRG5SRTE4JwzGtLuTbMbjYTqyEjXAkB\
-zo33a92znA0EXEeLCl845EUgzUkSkeN/T/uyWRjj0hrPU99UaaXHt3bc+lrDHrc7\
-WFAR3QoAfFFJPIqqwiHcBDdTeAozQ8IOqFIxspl72RukuRdeQR3EdfcF9TUZyUbU\
-k8SuRioggpkCgYA2scgnA3KvwYGKlKgxJc9fQ0zcGDlrw8E4BymPXsO9zs6hGAxb\
-TTfoYDJlGX361kli22zQpvdTK6/ZjQL+LfiyvTLHBeWRbVsPbfGwpp+9a9ZjYVnA\
-m1OeqIYo4Jc9TICNcZMzYTM6vkRVzwtrKw//mQpGsmNbGEilWvaciZHtoQKBgQDo\
-FDBQtir6SJIConm9/ETtBlLtai6Xj+lYnK6qC1DaxkLj6tjF9a9jVh3g/DfRopBW\
-ZnSCkpGkJcR54Up5s35ofCkdTdxPsmaLihuaje6nztc+Y8VS1LAIs41GunRkF/5s\
-KzbI8kIyfAitag+Toms+v93SLwIWNo27gh3lYOANSQKBgQDIidSO3fzB+jzJh7R0\
-Yy9ADWbBsLxc8u+sBdxmZBGl+l4YZWNPlQsnsafwcpJWT3le6N7Ri3iuOZw9KiGe\
-QDkc7olxUZZ3pshg+cOORK6jVE8v6FeUlLnxpeAWa4C4JDawGPTOBct6bVBl5sxi\
-7GaqDcEK1TSxc4cUaiiPDNNXQA==\
------END PRIVATE KEY-----", "-----BEGIN CERTIFICATE-----\
-MIIDhTCCAm2gAwIBAgIJANrO2RnCbURLMA0GCSqGSIb3DQEBCwUAMFkxCzAJBgNV\
-BAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBX\
-aWRnaXRzIFB0eSBMdGQxEjAQBgNVBAMMCWxvY2FsaG9zdDAeFw0xNzA0MjgxNTA0\
-NDVaFw0xODA0MjgxNTA0NDVaMFkxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21l\
-LVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQxEjAQBgNV\
-BAMMCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOv3\
-QeuzCASaWf+td42pirRugi9qznC7buGZ4cN0YYq2QJYhYYCj0ZTLjnGqwjiYOXds\
-5ZL6mZv/fKL6eF74GZge8SpQWjUl9RxUzFo0Rt131ETK0yMp05L6SQt/74EFGhkj\
-+DwOCAi75GJ05LKmT6lQQLJDFE6zIKsH8WBe4pvILhA2Xt+pnExKnPDuOxYJmLAW\
-/im7yQyrMMbbBOX+v/hJPIrg+PZNVr8lXNtlsftV9yNPwx0yxe8qGWY/LCNuqjvQ\
-eea3dydqRgVf7TudzH/maNhh3d5IWCI1evtY3DauEUq19NRyOupLD0UPGYcVZRTv\
-qbAnJSjTY31WJqRKhCkCAwEAAaNQME4wHQYDVR0OBBYEFPFfmGA3jO9koBZNGNZC\
-T/dZHZyHMB8GA1UdIwQYMBaAFPFfmGA3jO9koBZNGNZCT/dZHZyHMAwGA1UdEwQF\
-MAMBAf8wDQYJKoZIhvcNAQELBQADggEBAIc8Yuom4vz82izNEV+9bcCvuabcVwLH\
-Qgpv5Nzy/W+1hDoqfMfKNwOSdUB7jZoDaNDG1WhjKGGCLTAx4Hx+q1LwUXvu4Bs1\
-woocge65bl85h10l2TxxnlT5BIJezm5r3NiZSwOK2zxxIEyL4vh+b/xqQblBEkR3\
-e4/A4Ugn9Egh8GdpF4klGp4MjjpRyAVI7BDaleAhvDSfPmm7ylHJ2y7CLI9ApOQY\
-glwRuTmowAZQtaSiE1Ox7QtWj858HDzzTZyFWRG/MNqQptn7AMTPJv3DivNfDNPj\
-fYxFAheH3CjryHqqR9DD+d9396W8mqEaUp+plMwSjpcTDSR4rEQkUJg=\
------END CERTIFICATE-----");
+  if (argc > 3 && 0 == o_strcmp(argv[1], "-https")) {
+    key_file = read_file(argv[1]);
+    cert_file = read_file(argv[2]);
+    if (key_file == NULL || cert_file == NULL) {
+      printf("Error reading https certificate files\n");
+      ret = U_ERROR_PARAMS;
+    } else {
+      ret = ulfius_start_secure_framework(&instance, key_file, cert_file);
+    }
+    free(key_file);
+    free(cert_file);
   } else {
     ret = ulfius_start_framework(&instance);
   }
