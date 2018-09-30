73c646cc497ba41aee3c6d852cf385387b0551f9
Nicolas Mora <github@babelouest.org>
2018-09-30 15:26:35 -0400

    Fix cppcheck warning

--- uwsc/uwsc.c
+++ uwsc/uwsc.c
@@ -57,8 +57,8 @@ static char * read_file(const char * filename, size_t * filesize) {
       buffer = malloc (length + 1);
       if (buffer) {
         fread (buffer, 1, length, f);
+        buffer[length] = '\0';
       }
-      buffer[length] = '\0';
       fclose (f);
     }
     return buffer;
