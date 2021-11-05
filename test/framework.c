7341d1eb48ba250cb3880446a35c05f5c9110a8b
Nicolas Mora <github@babelouest.org>
2021-11-05 07:53:24 -0400

    Disable logs

--- test/framework.c
+++ test/framework.c
@@ -1653,7 +1653,7 @@ int main(int argc, char *argv[])
   int number_failed;
   Suite *s;
   SRunner *sr;
-  y_init_logs("Ulfius", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting Ulfius framework tests");
+  //y_init_logs("Ulfius", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting Ulfius framework tests");
   ulfius_global_init();
   s = ulfius_suite();
   sr = srunner_create(s);
@@ -1663,6 +1663,6 @@ int main(int argc, char *argv[])
   srunner_free(sr);
   
   ulfius_global_close();
-  y_close_logs();
+  //y_close_logs();
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
